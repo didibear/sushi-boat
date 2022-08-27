dfe47281779ca07da6b7533ba9793a35ff79e078
Adrien Turiot <adrien.turiot@maxa.ai>
2022-08-27 03:19:39 -0400

    feat: combine item in between them

--- src/main.rs
+++ src/main.rs
@@ -132,8 +132,7 @@ fn drag_and_drop_item(
 fn combine_items(
     mut commands: Commands,
     mut collision_events: EventReader<CollisionEvent>,
-    items: Query<&Item>,
-    mouse_position: Res<MousePosition>,
+    items: Query<(&Item, &Transform)>,
     mut grabbed_item: ResMut<GrabbedItem>,
 ) {
     let collisions = collision_events
@@ -145,17 +144,25 @@ fn combine_items(
         .map(sorted) // Remove double-counted collisions
         .collect::<HashSet<_>>();
 
-    for [item1, item2] in collisions {
-        match (
-            items.get_component::<Item>(*item1),
-            items.get_component::<Item>(*item2),
-        ) {
-            (Ok(Item::Rice), Ok(Item::SeaWeed)) | (Ok(Item::SeaWeed), Ok(Item::Rice)) => {
-                grabbed_item.0 = None;
-                commands.entity(*item1).despawn_recursive();
-                commands.entity(*item2).despawn_recursive();
+    let collided_items = collisions.into_iter().filter_map(|[e1, e2]| {
+        let [i1, i2] = items.get_many([*e1, *e2]).ok()?;
+        Some(((e1, i1), (e2, i2)))
+    });
 
-                spawn_item(&mut commands, Item::Onigiri, mouse_position.0);
+    for ((entity1, (item1, transform1)), (entity2, (item2, transform2))) in collided_items {
+        match (item1, item2) {
+            (Item::Rice, Item::SeaWeed) | (Item::SeaWeed, Item::Rice) => {
+                grabbed_item.0 = None;
+                commands.entity(*entity1).despawn_recursive();
+                commands.entity(*entity2).despawn_recursive();
+
+                let in_between_translation = (transform1.translation + transform2.translation) / 2.;
+
+                spawn_item(
+                    &mut commands,
+                    Item::Onigiri,
+                    in_between_translation.truncate(),
+                );
             }
             _ => {}
         }
