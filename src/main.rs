77ca216a796222132094c069260430d9b80a63ea
Adrien Turiot <adrien.turiot@maxa.ai>
2022-08-27 01:09:38 -0400

    feat: add drag and drop item system

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use bevy::{prelude::*, window::close_on_esc};
 use bevy_inspector_egui::WorldInspectorPlugin;
 use bevy_rapier2d::prelude::*;
 
+const Z: f32 = 0.;
+
 fn main() {
     App::new()
         .insert_resource(WindowDescriptor {
@@ -13,12 +15,14 @@ fn main() {
         .insert_resource(ClearColor(Color::BLACK))
         .insert_resource(Msaa::default())
         .insert_resource(Spawner(Timer::from_seconds(1., true)))
+        .insert_resource(GrabbedItem::default())
         .add_plugins(DefaultPlugins)
         .add_plugin(WorldInspectorPlugin::new())
         .add_plugin(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.))
         .add_plugin(RapierDebugRenderPlugin::default())
         .add_startup_system(setup_camera)
         .add_startup_system(setup_ground)
+        .add_system(drag_and_drop_item)
         .add_system(close_on_esc)
         .add_system(spawn_shapes)
         .run();
@@ -30,18 +34,19 @@ fn setup_camera(mut commands: Commands) {
 
 fn setup_ground(mut commands: Commands) {
     commands
-        .spawn_bundle(TransformBundle::from(Transform::from_xyz(0., -250., 0.)))
+        .spawn_bundle(TransformBundle::from(Transform::from_xyz(0., -250., Z)))
         .insert(Collider::cuboid(250., 20.));
 }
 
 struct Spawner(Timer);
 
+#[derive(Component)]
+struct Item;
+
 fn spawn_shapes(
     mut commands: Commands,
     mut spawner: ResMut<Spawner>,
     time: Res<Time>,
-    // mouse_button_input: Res<Input<MouseButton>>,
-    // windows: Res<Windows>,
 ) {
     if spawner.0.tick(time.delta()).just_finished() {
         let shape = match (time.seconds_since_startup() * 1000.) as i32 % 4 {
@@ -52,10 +57,56 @@ fn spawn_shapes(
         };
 
         commands
-            .spawn_bundle(TransformBundle::from(Transform::from_xyz(0., 100., 0.)))
+            .spawn()
+            .insert(Item)
+            .insert_bundle(TransformBundle::from(Transform::from_xyz(0., 100., 0.)))
             .insert(RigidBody::Dynamic)
             .insert(shape);
     }
-    // if mouse_button_input.pressed(MouseButton::Left) {
-    // let window = windows.primary().cursor_position();
+}
+
+#[derive(Debug, Default)]
+struct GrabbedItem(Option<Entity>);
+
+fn drag_and_drop_item(
+    windows: Res<Windows>,
+    mouse: Res<Input<MouseButton>>,
+    mut cursor_moved_events: EventReader<CursorMoved>,
+    mut last_mouse_position: Local<Vec2>,
+    mut items: Query<(Entity, &mut Transform, &Collider), With<Item>>,
+    mut grabbed_item: ResMut<GrabbedItem>,
+) {
+    if mouse.just_released(MouseButton::Left) {
+        grabbed_item.0 = None;
+        return;
+    }
+    if !mouse.pressed(MouseButton::Left) {
+        return;
+    }
+    let window = windows.primary();
+
+    let mouse_position = cursor_moved_events
+        .iter()
+        .last()
+        .map(|cursor| cursor.position - Vec2::new(window.width(), window.height()) / 2.)
+        .unwrap_or(*last_mouse_position);
+
+    *last_mouse_position = mouse_position;
+
+    if mouse.just_pressed(MouseButton::Left) {
+        grabbed_item.0 = items
+            .iter()
+            .find(|(_, transform, collider)| {
+                collider.contains_local_point(mouse_position - transform.translation.truncate())
+            })
+            .map(|(entity, ..)| entity);
+    }
+
+    if let Some(item) = grabbed_item.0 {
+        let mut transform = items
+            .get_component_mut::<Transform>(item)
+            .expect("items contains transform");
+
+        transform.translation = Vec3::from((mouse_position, Z));
+    }
 }
