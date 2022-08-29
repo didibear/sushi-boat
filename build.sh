4f08abe7d0a14c944b255f112dd8a1be54c1e0c1
Adrien Turiot <adrien.turiot@maxa.ai>
2022-08-29 10:14:04 -0400

    refactor: rename project to sushi-boat

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 cargo build --release --target wasm32-unknown-unknown
-wasm-bindgen --out-dir ./dist/target --target web ./target/wasm32-unknown-unknown/release/stacker.wasm
+wasm-bindgen --out-dir ./dist/target --target web ./target/wasm32-unknown-unknown/release/sushi-boat.wasm
 cp -r assets dist
